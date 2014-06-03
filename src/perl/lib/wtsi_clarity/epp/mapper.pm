4d462b2e50da06a828a50a86addbe8d6a1eb04fa
Chris Smith <cs24@sanger.ac.uk>
2014-06-03 13:41:05 +0100

    Added bed_verificaiton action to mapper

--- src/perl/lib/wtsi_clarity/epp/mapper.pm
+++ src/perl/lib/wtsi_clarity/epp/mapper.pm
@@ -18,6 +18,7 @@ Readonly::Hash my %ACTION2MODULE => (
     'fluidigm_request_volume' => 'sm::fluidigm_request_volume',
     'cherrypick_volume' => 'sm::cherrypick_volume',
     'plate_purpose' => 'sm::plate_purpose',
+	'bed_verification' => 'sm::bed_verification',
 );
 
 has 'action'  => (
