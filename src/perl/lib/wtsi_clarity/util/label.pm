a26c16d8cba0b31963aa6399730d7598aa0be89d
Chris Smith <cs24@sanger.ac.uk>
2014-07-17 17:56:35 +0100

    Added new label and print modules

---
+++ src/perl/lib/wtsi_clarity/util/label.pm
@@ -0,0 +1,159 @@
+package wtsi_clarity::util::label;
+
+use strict;
+use warnings;
+use Carp;
+use Readonly;
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(generateLabels);
+
+our $VERSION = '0.0';
+
+Readonly::Scalar my $USER_NAME_LENGTH => 12;
+
+sub generateLabels {
+  my $params = shift;
+  my $h = {};
+
+  $h->{'label_printer'}->{'header_text'} = {
+    header_text1 => 'header by ' . $params->{'user'},
+    header_text2 => $params->{'date'}->strftime('%a %b %d %T %Y'),
+  };
+
+  $h->{'label_printer'}->{'footer_text'} = {
+    footer_text1 => 'footer by ' .$params->{'user'},
+    footer_text2 => $params->{'date'}->strftime('%a %b %d %T %Y'),
+  };
+
+  my @labels = ();
+
+  foreach my $container_url (keys %{$params->{'containers'}}) {
+    my $count = 0;
+    while ($count < $params->{'number'}) {
+      my $container = $params->{'containers'}->{$container_url};
+      push @labels, _format_label($container, $params);
+      $count++;
+    }
+  }
+
+  $h->{'label_printer'}->{'labels'} = \@labels;
+
+  return $h;
+}
+
+sub _format_label {
+  my ($container, $params) = @_;
+
+  my $label;
+
+  my $type = $params->{'type'};
+  my $date = $params->{'date'}->strftime('%d-%b-%Y');
+  my $user = $params->{'source_plate'} ? q[] : $params->{'user'}; #no user for sample management stock plates  
+
+  if ($user && length $user > $USER_NAME_LENGTH) {
+    $user = substr $user, 0, $USER_NAME_LENGTH;
+  }
+  if ($type eq 'plate') {
+
+    $label = {
+        'template'  => 'plate',
+        'plate' => {
+          'ean13' => $container->{'barcode'},
+          'sanger' => join(q[ ], $date, $user),
+          'label_text' => {
+            'role'  => $container->{'purpose'},
+            'text5' => $container->{'num'},
+            'text6' => $container->{'signature'}
+          }
+        }
+    };
+
+  } elsif ($type eq 'tube') { #tube labels have not been tested yet
+    $label = {
+      'template' => 'tube',
+      'tube'     => {
+        'ean13' => $container->{'barcode'},
+        'sanger' => $container->{'lims_id'},
+        'label_text' => {
+          'role' => $user,
+        },
+    }};
+  } else {
+    croak qq[Unknown container type $type, known types: tube, plate];
+  }
+
+  return $label;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+wtsi_clarity::util::label
+
+=head1 SYNOPSIS
+
+    use wtsi_clarity::epp::label qw/generateLabels/;
+
+    generateLabels({
+      'number'       => $num_copies, 
+      'type'         => $container_type,
+      'user'         => $user,
+      'date'         => $date,
+      'containers'   => $container,
+      'source_plate' => $source_plate,
+    })
+
+=head1 DESCRIPTION
+
+ Label generation for plates and tubes
+
+=head1 SUBROUTINES/METHODS
+
+=head2 generateLabels
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item strict
+
+=item warnings
+
+=item Carp
+
+=item Exporter
+
+=item Readonly
+
+=back
+
+=head1 AUTHOR
+
+Carol Scott E<lt>ces@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 Genome Research Ltd.
+
+This file is part of wtsi_clarity project.
+
+wtsi_clarity is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ src/perl/lib/wtsi_clarity/util/print.pm
@@ -0,0 +1,118 @@
+package wtsi_clarity::util::print;
+
+use Moose::Role;
+use Carp;
+use JSON;
+
+use wtsi_clarity::util::request;
+
+requires 'config';
+
+our $VERSION = '0.0';
+
+has 'json_request' => (
+  isa => 'wtsi_clarity::util::request',
+  is  => 'ro',
+  traits => [ 'NoGetopt' ],
+  default => sub { return wtsi_clarity::util::request->new('content_type' => 'application/json'); },
+);
+
+sub print_labels {
+  my ($self, $printer_name, $template) = @_;
+
+  my $printer_url = $self->_get_printer_url($printer_name);
+
+  $self->json_request->post($printer_url, encode_json($template));
+
+  return;
+}
+
+sub _get_printer_url {
+  my ($self, $printer_name) = @_;
+
+  my $print_service = $self->config->printing->{'service_url'};
+  if (!$print_service) {
+    croak q[service_url entry should be defined in the printing section of the configuration file];
+  }
+  my $url =  "$print_service/label_printers/page\=1";
+  my $p;
+
+  my $output = $self->json_request->get($url);
+  my $text = decode_json($output);
+
+  foreach my $t ( @{$text->{'label_printers'}} ){
+    if ($t->{'name'} && $t->{'name'} eq $printer_name){
+      if (!$t->{'uuid'}) {
+        croak qq[No uuid for printer $printer_name]
+      }
+      $p = join q[/], $print_service, $t->{'uuid'};
+    }
+  }
+  if (!$p) {
+    croak qq[Failed to get printer $printer_name details from $url]
+  }
+  return $p;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+wtsi_clarity::util::print
+
+=head1 SYNOPSIS
+
+    with wtsi_clarity::util::print;
+
+    $self->print_labels($printer_name, $template);
+
+=head1 DESCRIPTION
+
+ Prints some labels to a printer
+
+=head1 SUBROUTINES/METHODS
+
+=head2 print_labels
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Moose::Role;
+
+=item Carp;
+
+=item JSON;
+
+=item wtsi_clarity::util::request;
+
+=back
+
+=head1 AUTHOR
+
+Carol Scott E<lt>ces@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 Genome Research Ltd.
+
+This file is part of wtsi_clarity project.
+
+wtsi_clarity is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
