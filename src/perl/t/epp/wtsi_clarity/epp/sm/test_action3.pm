aac5dfcba781b57e464f8361a6e39bb8d07c891e
Karel <ke4@sanger.ac.uk>
2014-07-21 13:19:39 +0100

    dummy action for the attribute test

--- src/perl/t/epp/wtsi_clarity/epp/sm/test_action3.pm
+++ src/perl/t/epp/wtsi_clarity/epp/sm/test_action3.pm
@@ -7,4 +7,18 @@ use Moose;
 
 extends 'wtsi_clarity::epp::sm::test_action';
 
+has 'test_action3_attr' => (
+  isa        => 'Str',
+  is         => 'ro',
+  required   => 1,
+);
+
+override 'run' => sub {
+  my $self= shift;
+  my $message = 'Run method from '. ref $self;
+  $self->epp_log( $message 
+    . 'test_action3_attr attribute value is '. $self->test_action3_attr
+  );
+};
+
 1;
