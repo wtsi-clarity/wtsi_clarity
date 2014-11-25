066e76064718095d9def2424a1f4f167713c178e
Chris Smith <cs24@sanger.ac.uk>
2014-11-25 11:36:14 +0000

    More message consumption
    Added message_handler class
    Added tests for thawing of message

---
+++ src/perl/lib/wtsi_clarity/mq/message_handler.pm
@@ -0,0 +1,61 @@
+package wtsi_clarity::mq::message_handler;
+
+use Moose;
+
+1;
+
+__END__
+
+=head1 NAME
+
+wtsi_clarity::mq::message_handler
+
+=head1 SYNOPSIS
+
+  my $message_handler = wtsi_clarity::mq::message_handler->new();
+  $message_handler->handle_message($json_string);
+
+=head1 DESCRIPTION
+
+ Handles messages coming off RabbitMQ. Dispatches them to relevant message enhancer.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 handle_message
+
+  Takes in JSON string. Converts to mq::message, and dispatches to relevant message enhancer.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Moose
+
+=back
+
+=head1 AUTHOR
+
+Chris Smith E<lt>cs24@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 GRL
+
+This file is part of wtsi_clarity project.
+
+wtsi_clarity is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
--- src/perl/t/30-mq-message_handler.t
+++ src/perl/t/30-mq-message_handler.t
@@ -1,13 +1,25 @@
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 7;
 
 use_ok('wtsi_clarity::mq::message_handler');
 
 {
   my $mq_consumer = wtsi_clarity::mq::message_handler->new();
   isa_ok($mq_consumer, 'wtsi_clarity::mq::message_handler');
+}
+
+{
+  my $mq_consumer = wtsi_clarity::mq::message_handler->new();
+  my $json_string = '{"__CLASS__":"wtsi_clarity::mq::message::epp-0.33","process_url":"http://clarity.com:1234","step_url":"http://clarity.com:1234/step","timestamp":"2014-11-25 12:06:27","purpose":"sample"}';
+  my $message = $mq_consumer->thaw($json_string);
+
+  isa_ok($message, 'wtsi_clarity::mq::message');
+  is($message->process_url, 'http://clarity.com:1234', 'Sets the process_url');
+  is($message->step_url, 'http://clarity.com:1234/step', 'Sets the step_url');
+  is($message->timestamp, '2014-11-25 12:06:27', 'Sets the timestamp');
+  is($message->purpose, 'sample', 'Sets the purpose');
 }
 
 1;
