42732b824e622cbd9fe2353b93a6aa8bb56b0d11
Chris Smith <cs24@sanger.ac.uk>
2015-07-01 15:40:06 +0100

    Fixes Bug #421
    Added a validator class for building validators.
    Added an EPP for validating a Fluidigm IFC chip barcodes.

---
+++ src/perl/lib/wtsi_clarity/epp/sm/fluidigm_barcode_validator.pm
@@ -0,0 +1,101 @@
+package wtsi_clarity::epp::sm::fluidigm_barcode_validator;
+
+use Moose;
+use Carp;
+use wtsi_clarity::util::validators qw/flgen_bc/;
+
+extends 'wtsi_clarity::epp';
+
+our $VERSION = '0.0';
+
+has 'output_barcode' => (
+  is => 'ro',
+  isa => 'Str',
+  lazy => 1,
+  builder => '_build_output_barcode',
+);
+
+sub _build_output_barcode {
+  my $self = shift;
+
+  if (scalar @{$self->process_doc->plate_io_map_barcodes} > 1) {
+    croak 'There should only be one Fluidigm 96.96 IFC plate';
+  }
+
+  return $self->process_doc->plate_io_map_barcodes->[0]{'dest_plate'};
+}
+
+override 'run' => sub {
+  my $self= shift;
+  super();
+
+  my $validation = flgen_bc($self->output_barcode);
+
+  if ($validation->failed) {
+    croak $validation->error_message;
+  }
+
+  return 1;
+};
+
+1;
+
+__END__
+
+=head1 NAME
+
+wtsi_clarity::epp::sm::fluidigm_barcode_validator
+
+=head1 SYNOPSIS
+
+  wtsi_clarity::epp:sm::fluidigm_barocode_validator->new(
+    process_url => 'http://my.com/processes/3345',
+  )->run();
+
+=head1 DESCRIPTION
+
+  Will look at the output plate's barcode and validate that it is a valid Fluidigm barcode. It will
+  throw an error if it is. It'll do nothing if not.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 run - executes the callback
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Moose
+
+=item Carp
+
+=item wtsi_clarity::util::validators
+
+=back
+
+=head1 AUTHOR
+
+Chris Smith E<lt>cs24@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2015 Genome Research Ltd.
+
+This file is part of wtsi_clarity project.
+
+wtsi_clarity is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ src/perl/t/10-util-validator.t
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+
+use Test::More tests => 16;
+
+use_ok('wtsi_clarity::util::validators', (qw/flgen_bc ean13_bc/));
+
+{
+  is(flgen_bc(1234567890)->failed, 0, 'Returns 1 for a valid Fluidigm barcode');
+
+  is(flgen_bc(123456789)->failed, 1, 'Returns 0 if barcode is too short');
+  is(flgen_bc(123456789)->error_message, 'Validation for value 123456789 failed. The barcode must have a length of 10.',
+    'Returns an error messages when barcode is too short');
+
+  is(flgen_bc(12345678901)->failed, 1,
+    'Returns 0 when the barcode is too long');
+  is(flgen_bc(12345678901)->error_message, 'Validation for value 12345678901 failed. The barcode must have a length of 10.',
+    'Returns an error messages when the barcode is too long');
+
+  is(flgen_bc('1234a56898')->failed, 1,
+    'Returns an 0 when the barcode is not numeric');
+  is(flgen_bc('1234a56898')->error_message, 'Validation for value 1234a56898 failed. The barcode must be numeric.',
+    'Returns an error message when the barcode is not numeric');
+
+  is(flgen_bc('1234567890')->failed, 0,
+    'Returns true when integers in a string are passed in');
+
+  is(flgen_bc('123_456_58')->failed, 1,
+    'Returns 0 when underscores are present');
+  is(flgen_bc('123_456_58')->error_message, 'Validation for value 123_456_58 failed. The barcode must be numeric.',
+    'Returns an error message when underscores are present');
+
+  is(flgen_bc('abc')->error_message, 'Validation for value abc failed. The barcode must have a length of 10. The barcode must be numeric.',
+    'Will concatenate multiple errors');
+}
+
+{
+  is(ean13_bc(1234567891234)->failed, 0, 'Returns 1 for a valid EAN13 barcode.');
+  is(ean13_bc(123)->error_message, 'Validation for value 123 failed. The barcode must have a length of 13.');
+  is(ean13_bc(123456789123456789)->error_message, 'Validation for value 123456789123456789 failed. The barcode must have a length of 13.');
+  is(ean13_bc('1234aaa654658')->error_message, 'Validation for value 1234aaa654658 failed. The barcode must be numeric.');
+}
+
+1;
