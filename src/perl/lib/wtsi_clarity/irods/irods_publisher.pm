51ce9e610313170db46b39cf6c081db55dd51eda
Karel <ke4@sanger.ac.uk>
2015-08-05 15:29:41 +0100

    added irons supporting module

---
+++ src/perl/lib/wtsi_clarity/irods/irods_publisher.pm
@@ -0,0 +1,133 @@
+package wtsi_clarity::irods::irods_publisher;
+
+use Moose;
+use Readonly;
+use Carp;
+
+our $VERSION = '0.0';
+
+Readonly::Scalar my $OVERWRITE_IF_EXISTS => 1;
+
+sub publish {
+  my ($self, $file, $destination, $is_overwrite, @metadata) = @_;
+
+  $self->_put($file, $destination, $OVERWRITE_IF_EXISTS);
+
+  if (@metadata) {
+    $self->_add_metadata_to_file($destination, @metadata);
+  }
+
+  return 1;
+}
+
+sub remove {
+  my ($self, $file) = @_;
+
+  my $irm_command = "irm $file";
+
+  return $self->_execute_irods_command($irm_command);
+}
+
+sub _put {
+  my ($self, $file, $destination, $is_overwrite) = @_;
+
+  my $iput_command = "iput -K $file $destination";
+
+  return $self->_execute_irods_command($iput_command);
+}
+
+sub _add_metadata_to_file {
+  my ($self, $file, @metadatum) = @_;
+  my $exit_code;
+
+  foreach my $metadata (@metadatum) {
+    my $imeta_command = "imeta add -d $file $metadata->{'attribute'} $metadata->{'value'}";
+    $exit_code = $self->_execute_irods_command($imeta_command);
+  }
+
+  return $exit_code;
+}
+
+sub _execute_irods_command {
+  my ($self, $command) = @_;
+
+  my $exit_code = system qq{$command > /dev/null 2>&1};
+
+  if ($exit_code != 0) {
+    croak qq{The following iRODS command has failed:\n$command};
+  }
+
+  return $exit_code;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+wtsi_clarity::irods::irods_publisher
+
+=head1 SYNOPSIS
+
+  wtsi_clarity::irods::irods_publisher->new(
+       process_url => 'http://clarity-ap:8080/processes/3345',
+       step_url    => 'http://testserver.com:1234/here/steps/24-98970',
+  )->run();
+
+=head1 DESCRIPTION
+
+  This module wraps around iRODS command to be able to publish file to the iRODS file system
+  and also be able to add metadat to it.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 publish
+  
+  Publish a file to iRODS with metadata.
+  Republish any file that is already published, but whose checksum has changed.
+
+=head2 remove
+
+  Remove the given file from iRODS. 
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Moose
+
+=item Carp
+
+=item Readonly
+
+=item XML::LibXML
+
+=back
+
+=head1 AUTHOR
+
+Karoly Erdos E<lt>ke4@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 Genome Research Ltd.
+
+This file is part of wtsi_clarity project.
+
+wtsi_clarity is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ src/perl/t/10-irods-irods_publisher.t
@@ -0,0 +1,79 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Exception;
+use DateTime;
+
+my $exit_code = system('ihelp > /dev/null 2>&1');
+
+if ($exit_code != 0) {
+  plan skip_all => 'iRODS icommands needs to be installed and they needs to be on the PATH.';
+} else {
+  plan tests => 12;
+}
+
+use_ok('wtsi_clarity::irods::irods_publisher');
+
+{
+  my $publisher = wtsi_clarity::irods::irods_publisher->new();
+  isa_ok($publisher, 'wtsi_clarity::irods::irods_publisher');
+}
+
+my $data_path = 't/data/irods/irods_publisher';
+my $test_file_name = 'test_file.txt';
+my $destination_folder = '/Sanger1-dev/home/glsai';
+
+my $file_to_put = "$data_path/$test_file_name";
+my $destination_path = "$destination_folder/$test_file_name";
+my $file_to_add_metadata = $destination_path;
+my $file_to_remove = $destination_path;
+
+my $overwrite = 1;
+
+my @metadatum = (
+  {
+    "attribute" => "type",
+    "value"     => "manifest.txt",
+  },
+  {
+    "attribute" => "time",
+    "value"     => DateTime->now(),
+  }
+);
+
+
+{ # add a file to iRODS
+  my $publisher = wtsi_clarity::irods::irods_publisher->new();
+
+  my $exit_code;
+
+  lives_ok {$exit_code = $publisher->_put($file_to_put, $destination_path, $overwrite)}
+    'Successfully put file to iRODS.';
+  is($exit_code, 0, "Successfully exited from the iput command.");
+  lives_ok {$exit_code = $publisher->_add_metadata_to_file($file_to_add_metadata, @metadatum)}
+    'Successfully add metadata to the file in iRODS.';
+  is($exit_code, 0, "Successfully exited from the imeta command.");
+
+  # cleanup
+  lives_ok {$exit_code = $publisher->remove($file_to_remove)}
+    'Successfully removed file from iRODS.';
+  is($exit_code, 0, "Successfully exited from the irm command.");
+}
+
+{
+  my $publisher = wtsi_clarity::irods::irods_publisher->new();
+
+  my $exit_code;
+
+  lives_ok {$exit_code = $publisher->publish($file_to_put, $destination_path, $overwrite, @metadatum)} 
+    'Successfully published the file into iRODS and added the metadata to it.';
+
+  is($exit_code, 1, "Successfully exited from the iput and imeta commands.");
+
+  # cleanup
+  lives_ok {$exit_code = $publisher->remove($file_to_remove)}
+    'Successfully removed file from iRODS.';
+  is($exit_code, 0, "Successfully exited from the irm command.");
+}
+
+1;
