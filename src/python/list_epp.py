b7ccdf8ce51369da384627f74825e0d1fc32833a
Ronan Forman <rf9@sanger.ac.uk>
2015-10-14 12:37:52 +0100

    Reformatted file, added data about when the script is activated.

--- src/python/list_epp.py
+++ src/python/list_epp.py
@@ -1,8 +1,8 @@
 import urllib.request as request
 from urllib.parse import urljoin
-from urllib.error import HTTPError
-import xml.etree.ElementTree as et
+from xml.etree import ElementTree
 import sys
+import getpass
 
 __author__ = 'rf9'
 
@@ -18,64 +18,67 @@ def setup_urllib():
     request.install_opener(opener)
 
 
-def get_all_protocols():
-    uri = urljoin(ROOT_URL, 'configuration/protocols/')
+def get_xml(uri):
     xml = request.urlopen(uri)
-    root = et.parse(xml).getroot()
-
-    return {protocol.get("name"): protocol.get("uri") for protocol in root.findall("protocol")}
+    return ElementTree.parse(xml).getroot()
 
 
-def get_processes_from_protocol(uri):
-    xml = request.urlopen(uri)
-    root = et.parse(xml).getroot()
-    process_types = [step.find('process-type') for step in root.find("steps").findall("step")]
-
-    return {process_type.text: process_type.get("uri") for process_type in process_types}
-
-
-def get_scripts_from_process(process_uri):
-    try:
-        xml = request.urlopen(process_uri)
-    except HTTPError:
-        return []
-
-    root = et.parse(xml).getroot()
-    params = root.findall("parameter")
-    string_objects = [param.find("string") for param in params]
-    strings = [string.text.strip() for string in string_objects if string is not None]
-
-    return [string for string in strings if len(string) > 0]
+def format_string(in_str):
+    return in_str.replace("_", " ").capitalize()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 3:
         ROOT_URL = sys.argv[1]
-        USER = sys.argv[2]
-        PASS = sys.argv[3]
+        OUT_FILE_PATH = sys.argv[2]
     else:
-        sys.stderr.write("usage: python list_epp.py <root_uri> <username> <password>\n")
+        sys.stderr.write("usage: python list_epp.py <root_uri> <output_file>\n")
         sys.exit(1)
 
     if ROOT_URL[-1] != '/':
         ROOT_URL += "/"
 
+    USER = getpass.getuser()
+    USER = input("Username (leave blank for '" + USER + "'): ") or USER
+    PASS = getpass.getpass('Password: ')
 
     setup_urllib()
 
-    print("Protocol", "Step", "Script", sep=SEP)
+    with open(OUT_FILE_PATH, 'w') as OUT_FILE:
+        OUT_FILE.write(SEP.join(["Protocol", "Step", "Script Name", "Behaviour", "Stage", "Timing", "Script"]) + "\n")
 
-    protocols = get_all_protocols()
-    for protocol in protocols:
-        processes = get_processes_from_protocol(protocols[protocol])
-        if processes:
-            for process in processes:
-                uri = processes[process]
-                scripts = get_scripts_from_process(uri)
-                if scripts:
-                    for script in scripts:
-                        print(protocol, process, script, sep=SEP)
-                else:
-                    print(protocol, process, "", sep=SEP)
-        else:
-            print(protocol, "", "", sep=SEP)
+        protocols = get_xml(urljoin(ROOT_URL, 'configuration/protocols/')).findall("protocol")
+        print("{:>3}/{}".format(0, len(protocols)))
+        for i, protocol in enumerate(protocols):
+            protocol_name = protocol.get("name")
+            protocol_uri = protocol.get("uri")
+
+            steps = get_xml(protocol_uri).find("steps").findall("step")
+            if steps:
+                for step in steps:
+                    step_name = step.get("name")
+                    process_type_uri = step.find("process-type").get("uri")
+
+                    epps = {}
+                    epp_triggers = step.find("epp-triggers").findall("epp-trigger")
+                    if epp_triggers:
+                        for epp_trigger in epp_triggers:
+                            epps[epp_trigger.get("name")] = {
+                                "type": epp_trigger.get("type") or "",
+                                "point": epp_trigger.get("point") or "",
+                                "status": epp_trigger.get("status") or "",
+                            }
+
+                        for script in get_xml(process_type_uri).findall("parameter"):
+                            string = script.find("string")
+                            epps[script.get("name")]["script"] = string.text.strip() if string is not None else ""
+
+                        for epp_name, epp in epps.items():
+                            OUT_FILE.write(SEP.join([protocol_name, step_name, epp_name, format_string(epp.get('type')),
+                                                     format_string(epp.get('status')), format_string(epp.get('point')),
+                                                     epp.get('script')]) + "\n")
+                    else:
+                        OUT_FILE.write(protocol_name + SEP + step_name + "\n")
+            else:
+                OUT_FILE.write(protocol_name + '\n')
+            print("{:>3}/{}".format(i + 1, len(protocols)))
