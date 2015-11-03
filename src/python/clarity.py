1243555cee8191a53c38ea8d2892af1df99c8517
Ronan Forman <rf9@sanger.ac.uk>
2015-11-03 16:49:47 +0000

    Added a clarity class to the python scripts to handle common functionality, added a batch_get_xml method, added a script to convert samples into their container barcodes.

---
+++ src/python/clarity.py
@@ -0,0 +1,63 @@
+import getpass
+import urllib.request as request
+from urllib.error import HTTPError
+from xml.etree import ElementTree
+
+__author__ = 'rf9'
+
+
+class ClarityException(Exception):
+    pass
+
+
+class Clarity:
+    def __init__(self, root):
+        if root[-1] != '/':
+            root += "/"
+        self.root = root
+
+        user = getpass.getuser()
+        user = input("Username (leave blank for '" + user + "'): ") or user
+        password = getpass.getpass('Password: ')
+
+        self.setup_urllib(user, password)
+
+    def setup_urllib(self, user, password):
+        password_mgr = request.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None, self.root, user, password)
+        handler = request.HTTPBasicAuthHandler(password_mgr)
+        opener = request.build_opener(handler)
+        opener.open(self.root)
+        request.install_opener(opener)
+
+    @staticmethod
+    def get_xml(uri):
+        xml = request.urlopen(uri)
+        return ElementTree.parse(xml).getroot()
+
+    def batch_get_xml(self, object_type, uri_list):
+        if object_type not in ('artifacts', 'containers', 'files', 'samples'):
+            raise ClarityException("Cannot batch %r" % object_type)
+
+        builder = ElementTree.TreeBuilder()
+        builder.start('ri:links', {
+            'xmlns:ri': "http://genologics.com/ri",
+        })
+        element = builder.close()
+
+        for uri in uri_list:
+            child = ElementTree.TreeBuilder().start('link', {
+                'uri': uri,
+                'rel': object_type,
+            })
+            element.append(child)
+
+        req = request.Request(url=self.root + object_type + '/batch/retrieve', data=ElementTree.tostring(element),
+                              method='POST')
+        req.add_header("Content-Type", "application/xml")
+
+        try:
+            with request.urlopen(req) as response:
+                return ElementTree.parse(response).getroot().getchildren()
+        except HTTPError:
+            raise Exception(ElementTree.tostring(element))
--- src/python/list_epp.py
+++ src/python/list_epp.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python3
 
-import urllib.request as request
 from urllib.parse import urljoin
-from xml.etree import ElementTree
+
 import sys
-import getpass
+
+from clarity import Clarity
 
 __author__ = 'rf9'
 
 SEP = ", "
-
-
-def setup_urllib():
-    password_mgr = request.HTTPPasswordMgrWithDefaultRealm()
-    password_mgr.add_password(None, ROOT_URL, USER, PASS)
-    handler = request.HTTPBasicAuthHandler(password_mgr)
-    opener = request.build_opener(handler)
-    opener.open(ROOT_URL)
-    request.install_opener(opener)
-
-
-def get_xml(uri):
-    xml = request.urlopen(uri)
-    return ElementTree.parse(xml).getroot()
 
 
 def format_string(in_str):
@@ -31,31 +17,24 @@ def format_string(in_str):
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-        ROOT_URL = sys.argv[1]
+        root_url = sys.argv[1]
         OUT_FILE_PATH = sys.argv[2]
     else:
         sys.stderr.write("usage: python list_epp.py <root_uri> <output_file>\n")
         sys.exit(1)
 
-    if ROOT_URL[-1] != '/':
-        ROOT_URL += "/"
-
-    USER = getpass.getuser()
-    USER = input("Username (leave blank for '" + USER + "'): ") or USER
-    PASS = getpass.getpass('Password: ')
-
-    setup_urllib()
+    clarity = Clarity(root_url)
 
     with open(OUT_FILE_PATH, 'w') as OUT_FILE:
         OUT_FILE.write(SEP.join(["Protocol", "Step", "Script Name", "Behaviour", "Stage", "Timing", "Script"]) + "\n")
 
-        protocols = get_xml(urljoin(ROOT_URL, 'configuration/protocols/')).findall("protocol")
+        protocols = clarity.get_xml(urljoin(root_url, 'configuration/protocols/')).findall("protocol")
         print("{:>3}/{}".format(0, len(protocols)))
         for i, protocol in enumerate(protocols):
             protocol_name = protocol.get("name")
             protocol_uri = protocol.get("uri")
 
-            steps = get_xml(protocol_uri).find("steps").findall("step")
+            steps = clarity.get_xml(protocol_uri).find("steps").findall("step")
             if steps:
                 for step in steps:
                     step_name = step.get("name")
@@ -71,7 +50,7 @@ if __name__ == "__main__":
                                 "status": epp_trigger.get("status") or "",
                             }
 
-                        for script in get_xml(process_type_uri).findall("parameter"):
+                        for script in clarity.get_xml(process_type_uri).findall("parameter"):
                             string = script.find("string")
                             epps[script.get("name")]["script"] = string.text.strip() if string is not None else ""
 
